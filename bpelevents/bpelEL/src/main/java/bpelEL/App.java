278bd3bb8d021949f21cf1a3121bc4bcbcd0036f
Pubudu Gunatilaka <pubudu538@gmail.com>
2014-05-23 19:53:38 +0530

    bpel events

--- bpelevents/bpelEL/src/main/java/bpelEL/App.java
+++ bpelevents/bpelEL/src/main/java/bpelEL/App.java
@@ -1,111 +1,108 @@
 package bpelEL;
 
-
 import java.util.ArrayList;
 import java.util.Properties;
-
 import org.apache.ode.bpel.common.ProcessState;
 import org.apache.ode.bpel.evt.BpelEvent;
-import org.apache.ode.bpel.evt.ProcessInstanceEvent;
-import org.apache.ode.bpel.evt.ProcessTerminationEvent;
+//import org.apache.ode.bpel.evt.ProcessInstanceEvent;
+//import org.apache.ode.bpel.evt.ProcessTerminationEvent;
 import org.apache.ode.bpel.evt.ProcessInstanceStateChangeEvent;
 import org.apache.ode.bpel.iapi.BpelEventListener;
-//import org.wso2.carbon.bpel.b4p.coordination.configuration.CoordinationConfiguration;
-//import org.wso2.carbon.bpel.b4p.internal.B4PContentHolder;
-//import org.wso2.carbon.bpel.b4p.internal.B4PServiceComponent;
-//import org.wso2.carbon.bpel.core.ode.integration.store.ProcessConfigurationImpl;
 
-public class App implements BpelEventListener
-{
+public class App implements BpelEventListener {
+
 	public void onEvent(BpelEvent bpelEvent) {
-		
-           if (bpelEvent instanceof ProcessTerminationEvent) {
 
-                ProcessTerminationEvent event = (ProcessTerminationEvent) bpelEvent;
-              //  ProcessConfigurationImpl processConf = getProcessConfiguration(event);
-                
-                System.out.println("Event Notification for termination ****** : " +
-    	                bpelEvent.getType().toString() +
-    	                ", " + bpelEvent.getTimestamp()+
-    	                ", event details " + bpelEvent.toString());
-                
-//                if (processConf.isB4PTaskIncluded()) {
-//                                       
-                	System.out.println("process id **** "+event.getProcessInstanceId());
-//                    
-//                    TerminationTask terminationTask = new TerminationTask(Long.toString(event.getProcessInstanceId()));
-//                    B4PContentHolder.getInstance().getCoordinationController().runTask(terminationTask);
-//                }
+		if (bpelEvent instanceof ProcessInstanceStateChangeEvent) {
+			
+			ProcessInstanceStateChangeEvent instanceStateChangeEvent = (ProcessInstanceStateChangeEvent) bpelEvent;
+			String state = "";
 
-            } else if (bpelEvent instanceof ProcessInstanceStateChangeEvent) {
-            	
-            	System.out.println("Event Notification for statechange ############# : " +
-    	                bpelEvent.getType().toString() +
-    	                ", " + bpelEvent.getTimestamp()+
-    	                ", event details " + bpelEvent.toString());
-            	
-                ProcessInstanceStateChangeEvent instanceStateChangeEvent = (ProcessInstanceStateChangeEvent) bpelEvent;
-                
-                             
-                if(ProcessState.STATE_COMPLETED_OK == instanceStateChangeEvent.getNewState())
-                {
-                	System.out.println("Event Notification for AllStates ############# : " +
-        	                bpelEvent.getType().toString() +
-        	                ", " + bpelEvent.getTimestamp()+
-        	                ", event details " + bpelEvent.toString());
-                	
-                	System.out.println("Process Instance Id - " +
-                	instanceStateChangeEvent.getProcessInstanceId()+" & Process id - " +
-                			instanceStateChangeEvent.getProcessId()+ " & Process Name " +
-                			instanceStateChangeEvent.getProcessName()+ " & time - "+
-                			instanceStateChangeEvent.getTimestamp() + " & type - "+
-                			instanceStateChangeEvent.getType() + " & toString - "+ 
-                			instanceStateChangeEvent.toString());
-                }
-            }
-           
-           if(bpelEvent.getType().toString()=="activityLifecycle")
-           {
-        	   ArrayList<String> instanceInfo = new ArrayList<String>();
-        	   
-        	   System.out.println("Pub noti for activity &&&& & & & & & &  & &&&&&&& : " +
-   	                bpelEvent.getType().toString() +
-   	                ", " + bpelEvent.getTimestamp()+
-   	                ", event details " + bpelEvent.toString());
-        	   
-        	   String[] info = bpelEvent.toString().split("\n");
-        	   instanceInfo.add(info[1].trim());
-        	   
-        	         for(int k=2;k<info.length;k++)
-        	         {
-        	        	 String[] values = info[k].split("=");
-        	        	 instanceInfo.add(values[1].trim());        	        	 
-        	         }        	  
-        	         
-        	         // arraylist is ready to publish
-        	         for (int i = 0; i < instanceInfo.size(); i++) {
-        	        	 System.out.println("Splitted event ----------# "+instanceInfo.get(i));
-					}
-           }
-           
-          
-          
-        
-    }
+			if (ProcessState.STATE_READY == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Ready";
+				setProcessDetails(instanceStateChangeEvent, state);
 
-    public void startup(Properties properties) {
-       // this.isCoordinationEnabled = CoordinationConfiguration.getInstance().isEnable();
-    }
+			} else if (ProcessState.STATE_ACTIVE == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Active";
+				setProcessDetails(instanceStateChangeEvent, state);
 
-    public void shutdown() {
-    }
+			} else if (ProcessState.STATE_COMPLETED_OK == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Completed";
+				setProcessDetails(instanceStateChangeEvent, state);
 
-//    private ProcessConfigurationImpl getProcessConfiguration(ProcessInstanceEvent event) {
-//        int tenantId = B4PServiceComponent.getBPELServer().getMultiTenantProcessStore().getTenantId(event.getProcessId());
-//        return (ProcessConfigurationImpl) B4PServiceComponent.
-//                getBPELServer().getMultiTenantProcessStore().getTenantsProcessStore(tenantId).
-//                getProcessConfiguration(event.getProcessId());
-//    }
-	
-	
+			} else if (ProcessState.STATE_COMPLETED_WITH_FAULT == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Failed";
+				setProcessDetails(instanceStateChangeEvent, state);
+
+			} else if (ProcessState.STATE_SUSPENDED == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Suspended";
+				setProcessDetails(instanceStateChangeEvent, state);
+
+			} else if (ProcessState.STATE_TERMINATED == instanceStateChangeEvent
+					.getNewState()) {
+				state = "Terminated";
+				setProcessDetails(instanceStateChangeEvent, state);
+
+			}
+
+		}
+
+		if (bpelEvent.getType().toString() == "activityLifecycle") {
+
+			ArrayList<String> instanceInfo = new ArrayList<String>();
+
+			System.out
+					.println("Pub noti for activity &&&& & & & & & &  & &&&&&&& : "
+							+ bpelEvent.getType().toString()
+							+ ", "
+							+ bpelEvent.getTimestamp()
+							+ ", event details "
+							+ bpelEvent.toString());
+
+			String[] info = bpelEvent.toString().split("\n");
+			instanceInfo.add(info[1].trim());
+
+			for (int k = 2; k < info.length; k++) {
+				String[] values = info[k].split("=");
+				instanceInfo.add(values[1].trim());
+			}
+
+			// arraylist is ready to publish
+			for (int i = 0; i < instanceInfo.size(); i++) {
+				System.out.println("Splitted event ----------# "
+						+ instanceInfo.get(i));
+			}
+		}
+
+	}
+
+	public void startup(Properties properties) {
+
+	}
+
+	public void shutdown() {
+	}
+
+	public void setProcessDetails(ProcessInstanceStateChangeEvent process,
+			String state) {
+		String processInstanceId = process.getProcessInstanceId().toString();
+		;
+		String processId = process.getProcessId().toString();
+		String processName = process.getProcessName().toString();
+		String timestamp = process.getTimestamp().toString();
+		String[] results = processId.split("\\}");
+		String packageName = results[results.length - 1];
+
+		System.out.println("Package Name - " + packageName
+				+ ", Process Name - " + processName + ", Process Id - "
+				+ processId + ", Instance Id - " + processInstanceId
+				+ ", timestamp - " + timestamp + ", State - " + state);
+
+	}
+
 }
